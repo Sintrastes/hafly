bd78196b6f71b7a2ebcb761edf78b2bd22f450c7
Nathan Bedell <nbedell@tulane.edu>
2022-05-20 20:10:12 -0400

    Initial implementation.

--- app/Main.hs
+++ app/Main.hs
@@ -1,4 +1,95 @@
+
 module Main where
 
+import Data.Dynamic
+import Data.Typeable
+import Data.HashMap
+import Prelude hiding (lookup)
+import Control.Concurrent (yield)
+import Data.Maybe
+import Type.Reflection (SomeTypeRep(..))
+
+data InterpreterData = InterpreterData {
+    exprDefs :: Map String Dynamic
+}
+
+data Ast =
+    Atom String
+  | Literal LiteralExpr
+  | App Ast Ast
+  | Sequence SequenceAst
+
+data LiteralExpr = 
+    IntLit Int
+  | DoubleLit Double
+  | StringLit String
+  | Record (Map String LiteralExpr)
+
+data SequenceAst = SequenceAst [SequenceExpr]
+
+data SequenceExpr = 
+      Expr Ast
+    | BindExpr String Ast
+
+type TypeError = String
+
+interpret :: InterpreterData -> Ast -> Either TypeError Dynamic
+interpret ctx@(InterpreterData exprDefs) = \case
+    Atom x  -> maybe (Left "Could not find atom") Right $ 
+        lookup x exprDefs
+    App f' x' -> do
+        f <- interpret ctx f'
+        x <- interpret ctx x'
+
+        let argType = case x of 
+                Dynamic z _ -> SomeTypeRep z
+        let functionType = case f of 
+                Dynamic z _ -> SomeTypeRep z
+        
+        case funResultTy functionType argType of
+            Nothing -> Left $ "Cannot apply function of type " ++ 
+                show functionType ++ " to argument of type " ++
+                    show argType
+            Just resultType -> pure $ fromJust $ 
+                dynApply f x
+    Literal lit -> pure $ interpretLit lit
+    _       -> undefined
+
+interpretLit :: LiteralExpr -> Dynamic
+interpretLit = \case
+      (IntLit n) -> toDyn n
+      (DoubleLit x) -> toDyn x
+      (StringLit s) -> toDyn s
+      (Record map) -> toDyn map
+
+
+interpretSequence :: InterpreterData -> SequenceAst -> Either TypeError Dynamic
+interpretSequence = undefined
+
+exampleContext = InterpreterData {
+    exprDefs = fromList 
+        [
+          ("printLn", toDyn putStrLn)
+        , ("+", toDyn ((+) :: Int -> Int -> Int))
+        , ("readLn", toDyn getLine)
+        ]
+}
+
+exampleAst = App (Atom "printLn") 
+    (Literal $ StringLit "Hello, world!")
+
+interpretIO :: InterpreterData -> Ast -> IO ()
+interpretIO ctx ast = do
+    let result = interpret ctx ast
+    case result of
+        Left err -> putStrLn err
+        Right x -> 
+            case fromDynamic @(IO ()) x of
+                Nothing -> putStrLn "Error: Expression was not of type IO ()"
+                Just action -> action
+
 main :: IO ()
-main = putStrLn "Hello, Haskell!"
+main = interpretIO 
+    exampleContext 
+    exampleAst
+    
