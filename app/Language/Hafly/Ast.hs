ec0f3b12d5704f0b1964b960284b7ab9f092cb22
Nathan Bedell <nbedell@tulane.edu>
2022-05-21 20:06:42 -0400

    Implement basic parser.

--- app/Language/Hafly/Ast.hs
+++ app/Language/Hafly/Ast.hs
@@ -1,26 +1,34 @@
+{-# OPTIONS_GHC -Wno-unrecognised-pragmas #-}
+{-# HLINT ignore "Use newtype instead of data" #-}
 
 module Language.Hafly.Ast where
 import Data.HashMap
+
+newtype Program = Program (Map String Ast)
 
 data Ast =
     Atom String
   | Literal LiteralExpr
   | App Ast Ast
   -- I'm not sure if this is nescesarry or not with Dynamic.
-  | Lambda (Ast -> Ast)
+  -- | Lambda (Ast -> Ast)
   | Sequence SequenceAst
-  | Var String
+  | Var String 
+        deriving(Show)
 
 data LiteralExpr =
     IntLit Int
   | DoubleLit Double
   | StringLit String
   | Record (Map String LiteralExpr)
+        deriving(Show)
 
 data SequenceAst = SequenceAst [SequenceExpr]
+    deriving(Show)
 
 data SequenceExpr =
       Expr Ast
     | BindExpr String Ast
+        deriving(Show)
 
 type TypeError = String
--- app/Language/Hafly/Parser.hs
+++ app/Language/Hafly/Parser.hs
@@ -1,37 +1,98 @@
 
-module Language.Hafly.Parser where
-import Language.Hafly.Ast (LiteralExpr, SequenceAst, Ast)
-import Text.Megaparsec
+module Language.Hafly.Parser(
+    parseExpression,
+    parseProgram
+) where
+import Language.Hafly.Ast
+import Text.Megaparsec hiding (token)
 import Data.Void
 import qualified Data.Text as T
+import Text.Megaparsec.Char
+import Control.Monad.Combinators.Expr
 
 type Parser = Parsec Void T.Text
 
+parseExpression :: T.Text -> Either (ParseErrorBundle T.Text Void) Ast
+parseExpression = parse (expr <* eof) ""
+
+parseProgram :: T.Text -> Either (ParseErrorBundle T.Text Void) Program
+parseProgram = parse program ""
+
+whitespace :: Parser ()
+whitespace = do
+    many $
+        try (char ' ' >> pure ()) <|>
+            (char '\n' >> pure ())
+    pure ()
+
+token p = do
+    whitespace
+    res <- p
+    whitespace
+    pure res
+
+leftBracket = token (char '{')
+
+rightBracket = token (char '}')
+
+bindToken = token (string "<-")
+
+semicolon = token (char ';')
+
+identifier = token (some alphaNumChar)
+
 -- Parser for an entire hafly program.
 program :: Parser Program
-program = undefined 
+program = undefined
 
 -- | Parse an arbitrary hafly expression.
 expr :: Parser Ast
-expr = undefined
+expr = try app
+  <|> baseExpr
+
+baseExpr :: Parser Ast
+baseExpr = try (Literal <$> literal)
+    <|> try (Atom <$> identifier)
+    <|> (Sequence <$> block)
 
 -- Parse a literal expression
 literal :: Parser LiteralExpr
-literal = undefined
+literal = try intLit <|>
+    stringLit
 
--- Parse a variable
--- Note: Will need to figure out from context which of these are bound/unbound.
-var :: Ast
-var = undefined
+intLit = token $ 
+    IntLit . read <$> some digitChar
+
+stringLit = token $ do
+    char '"'
+    x <- many $ noneOf ['"']
+    char '"'
+    return $ StringLit x
 
 -- Parse a sequntial block.
 block :: Parser SequenceAst
-block = undefined
+block = do
+    leftBracket
+    res <- sepBy1 sequenceExpr (token semicolon)
+    rightBracket
+    return $ SequenceAst res
+
+sequenceExpr :: Parser SequenceExpr
+sequenceExpr = try bindExpr 
+    <|> Expr <$> expr
+
+bindExpr :: Parser SequenceExpr
+bindExpr = do
+    x <- identifier
+    bindToken
+    y <- expr
+    return $ BindExpr x y
 
 -- Parse a function application.
-app :: Ast
-app = undefined
+app :: Parser Ast
+app = foldl1 App <$>
+    some baseExpr
 
 -- Parse an expression with operators.
-opExpr :: Ast 
-opExpr = undefined
+opExpr :: Parser Ast
+opExpr = makeExprParser baseExpr undefined
