24e533e71325853b7701402604eb6c4d823afbbb
Nathan Bedell <nbedell@tulane.edu>
2023-02-04 22:10:47 -0700

    Add stubs for parser test cases.

--- tests/Tests.hs
+++ tests/Tests.hs
@@ -1,2 +1,20 @@
 
-main = return ()
+import Test.Tasty
+import Test.Tasty.HUnit
+
+main = defaultMain $ testGroup "Hafly Test Suite" 
+    [
+      testCase "Parse string literal" $ do
+        return ()
+    , testCase "Parse string literal with simple quote 1" $ do
+        return ()
+    , testCase "Parse string literal with simple quote 2" $ do
+        return ()
+    -- Currently this would fail
+    , testCase "Parse string literal with complex quote" $ do
+        return ()
+    , testCase "Parse lambda expression" $ do
+        return ()
+    , testCase "Parse if-else expression" $ do
+        return ()
+    ]
