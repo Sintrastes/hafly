11f6ddee419fa1beb69f5f3803ddb4171e4b3d41
Nathan Bedell <nbedell@tulane.edu>
2023-02-05 19:39:38 -0700

    Make hafly.nix only expose library.

--- hafly.nix
+++ hafly.nix
@@ -6,13 +6,13 @@ mkDerivation {
   version = "0.1.0.0";
   src = ./.;
   isLibrary = true;
-  isExecutable = true;
+  isExecutable = false;
   libraryHaskellDepends = [
     base hashmap megaparsec multimap parser-combinators text
   ];
-  executableHaskellDepends = [
-    base hashmap haskeline megaparsec multimap parser-combinators text tasty tasty-hunit
-  ];
+  # executableHaskellDepends = [
+  #   base hashmap haskeline megaparsec multimap parser-combinators text tasty tasty-hunit
+  # ];
   license = "unknown";
   hydraPlatforms = lib.platforms.none;
 }
